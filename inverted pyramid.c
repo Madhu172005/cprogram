4f90549b3e4643d178c40fd7fe58ea1563a18541
Madhu172005 <128420738+Madhu172005@users.noreply.github.com>
2023-06-05 00:08:53 +0530

    Add files via upload

---
+++ inverted pyramid.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int rows,space,i,j;
+    printf("enter the no. of rows:\n");
+    scanf("%d",&rows);
+    for(i=rows;i>=1;--i)
+    {
+        for(space=0;space<rows-1;++space)
+        printf("");
+        for (j=i;j<2*i-1;++j)
+        printf("*");
+        for(j=0;j<i-1;++j)
+            printf("*");
+        printf("\n");
+
+    }
+    return 0;
+}
