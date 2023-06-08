b941d11b5515b25f94a0efe2d50dc31073910b23
Madhu172005 <128420738+Madhu172005@users.noreply.github.com>
2023-06-08 20:10:05 +0530

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int num1,num2,sum;
+    printf("enter num1:");
+    scanf("%d",&num1);
+    printf("enter num2:");
+    scanf("%d",&num2);
+    sum=num1+num2;
+    printf("sum=%d",sum);
+    return 0;
+}
